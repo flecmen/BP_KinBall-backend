54c76a1b2aacba2fadbac557d4866b50d7096c6f
David Flek <davidovkyflekovky@gmail.com>
2023-02-18 18:58:47 +0100

    swagger init

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,47 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import Logger from './lib/logger';
 import morganMiddleware from './config/morganMiddleware'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc';
 
+//uložení .env proměnných do process.env
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+// Swagger settings
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Kin-ball API",
+            version: "1.0.0",
+            description: "Dokumentace API webové aplikace Kin-Ball Institutu v Brně"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ],
+    },
+    apis: ["./routes/*.ts"]
+}
+const specs = swaggerJsDoc(options)
 
-app.use(morganMiddleware)
+const app: Express = express();
+
+// Swagger endpoint
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
+
+app.use(morganMiddleware) // Logger
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+app.use('/user', () => import('./routes/user-router'))
+
+
+const port = process.env.PORT;
 app.listen(port, () => {
     Logger.info(`Server is running at http://localhost:${port}`);
 });
---
+++ src/routes/user-router.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import { Prisma } from "@prisma/client";
+
+const router = express.Router();
+
+export default router;
