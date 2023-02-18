3d6de57e596edee55c9628071b81d2d9a733dddc
David Flek <davidovkyflekovky@gmail.com>
2023-02-18 17:49:49 +0100

    logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import Logger from './lib/logger';
+import morganMiddleware from './config/morganMiddleware'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(morganMiddleware)
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    Logger.info(`Server is running at http://localhost:${port}`);
 });
